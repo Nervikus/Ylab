1ab6e5248b8668dc05175ac2a43f8ab59804191f
Nervikus <egiarmen@gmail.com>
2023-03-12 23:29:05 +0300

    Sequences corrected

--- homework2/sequences/SequencesTest.java
+++ homework2/sequences/SequencesTest.java
@@ -4,18 +4,22 @@ package sequences;
 public class SequencesTest {
     public static void main(String[] args) {
         //сначала использовал scanner, но потом убрал, т.к. его не было в ТЗ
+        System.out.println("Введите число от 1 до 20:");
         int n = 10;
-
-        Sequences sequences = new SequencesImpl();
-        sequences.a(n);
-        sequences.b(n);
-        sequences.c(n);
-        sequences.d(n);
-        sequences.e(n);
-        sequences.f(n);
-        sequences.g(n);
-        sequences.h(n);
-        sequences.i(n);
-        sequences.j(n);
+        if (n > 0 && n <= 20) {
+            Sequences sequences = new SequencesImpl();
+            sequences.a(n);
+            sequences.b(n);
+            sequences.c(n);
+            sequences.d(n);
+            sequences.e(n);
+            sequences.f(n);
+            sequences.g(n);
+            sequences.h(n);
+            sequences.i(n);
+            sequences.j(n);
+        } else {
+            System.out.println("Ввод неверный");
+        }
     }
 }
