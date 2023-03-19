d61297645a0192b8c32809a1642dbee588624e10
Nervikus <egiarmen@gmail.com>
2023-03-19 22:05:09 +0300

    FileSort update: check count lines in Validator

--- homework3/filesort/Test.java
+++ homework3/filesort/Test.java
@@ -2,15 +2,21 @@ package filesort;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        File dataFile = new Generator().generate("data.txt", 100_000);
+        System.out.println(new Date());
+        File dataFile = new Generator().generate("data.txt", 1_000_000);
         System.out.println(new Validator(dataFile).isSorted()); // false
+        System.out.println();
+
         File sortedFile = new Sorter().sortFile(dataFile);
         System.out.println(new Validator(sortedFile).isSorted()); // true
+
         long end = System.currentTimeMillis();
+        System.out.println(new Date());
         System.out.println(end - start);
     }
 }
